1857098b67821a8f08e237c0909890ae060d9049
Daren <darenminarolli@gmail.com>
2023-11-10 14:23:14 +0100

    changes

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google'
+import { Azeret_Mono } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const mono = Azeret_Mono({ subsets: ['latin'],weight:['400'],})
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={mono.className}>{children}</body>
     </html>
   )
 }
