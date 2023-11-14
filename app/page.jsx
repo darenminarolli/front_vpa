25c4a9bc77b6a5a3c92e995403119de8e6f0ebb7
Daren <darenminarolli@gmail.com>
2023-11-14 15:30:39 +0100

    kk

--- app/page.jsx
+++ app/page.jsx
@@ -74,6 +74,24 @@ export default function Home() {
             </div>
             <div>
               <hgroup className='flex gap-x-3'>
+                <FaSearchLocation />
+                <h1 className='underline'> Adresa</h1>
+              </hgroup><br />
+              <div className='w-full'>
+
+              <iframe 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2996.243038400884!2d19.809122085571275!3d41.32532820044899!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x135031fb35995b7d%3A0xa642076c7a94ef9d!2sMy%20Gym!5e0!3m2!1sen!2s!4v1699961567097!5m2!1sen!2s" 
+              width="500" 
+              height="350" 
+              className='border-solid rounded-md  w-full md:w-[500px]' 
+              allowfullscreen=""
+              loading="lazy" 
+              referrerpolicy="no-referrer-when-downgrade">
+              </iframe>
+              </div>
+            </div>
+            <div>
+              <hgroup className='flex gap-x-3'>
                 <FaPhoneAlt />
                 <h1 className='underline'> Kontakt</h1>
               </hgroup><br />
@@ -107,37 +125,10 @@ export default function Home() {
 
         
     </div>
-              {/* <form className=''>
-                <label htmlFor="name">Emri:</label><br />
-                <input type="text" name="name" id="name" placeholder='Emri...'/><br />
-                <label htmlFor="lastname">Mbiemri:</label><br />
-                <input type="text" name="lastname" id="lastname" placeholder='Mbiemri...'/><br />
-                <label htmlFor="email">E-mail:</label><br />
-                <input type="email" name="email" id="email" placeholder='E-mail...'/><br />
-                <label htmlFor="number">Tel:</label><br />
-                <input type="number" name="number" id="number" placeholder='Numri...' /><br />
-                <button type="submit">Dergo</button>
-              </form> */}
+           
               </div>
             </div>
-            <div>
-              <hgroup className='flex gap-x-3'>
-                <FaSearchLocation />
-                <h1 className='underline'> Adresa</h1>
-              </hgroup><br />
-              <div className='w-full'>
-
-              <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2996.243038400884!2d19.809122085571275!3d41.32532820044899!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x135031fb35995b7d%3A0xa642076c7a94ef9d!2sMy%20Gym!5e0!3m2!1sen!2s!4v1699961567097!5m2!1sen!2s" 
-              width="500" 
-              height="350" 
-              className='border-solid rounded-md  w-full md:w-[500px]' 
-              allowfullscreen=""
-              loading="lazy" 
-              referrerpolicy="no-referrer-when-downgrade">
-              </iframe>
-              </div>
-            </div>
+            
            
           </div>
         </section>
