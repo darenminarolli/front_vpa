717db33388d0128300b0d538b3b77b9e3be8b4ef
Daren <darenminarolli@gmail.com>
2023-11-16 16:41:03 +0100

    elegant scrollbar & shadow for adress

--- app/components/Adress.jsx
+++ app/components/Adress.jsx
@@ -14,7 +14,7 @@ const Adress=()=>{
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2996.243038400884!2d19.809122085571275!3d41.32532820044899!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x135031fb35995b7d%3A0xa642076c7a94ef9d!2sMy%20Gym!5e0!3m2!1sen!2s!4v1699961567097!5m2!1sen!2s" 
               width="500" 
               height="350" 
-              className='border-solid rounded-md  w-full ' 
+              className='border-solid rounded-md  w-full adress ' 
               allowfullscreen=""
               loading="lazy" 
               referrerpolicy="no-referrer-when-downgrade">
